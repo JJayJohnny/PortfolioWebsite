d25d680368edab569e72b6ff3e32e3d1c705a7ac
JJayJohnny <jasiek.barczewski@gmail.com>
2023-08-26 18:14:07 +0200

    fix theme being undefined on refresh

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider enableSystem={true} attribute='class'>
+        <ThemeProvider enableSystem={true} attribute='class' defaultTheme='system'>
           <Navbar/>
           {children}
         </ThemeProvider>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {Link} from "react-scroll/modules"
 import {useTheme} from "next-themes"
 import {RiMoonFill, RiSunLine} from "react-icons/ri"
@@ -21,11 +21,19 @@ const NAV_ITEMS = [
 ]
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false)
+    const [mounted, setMounted] = useState(false)
     const {systemTheme, theme, setTheme} = useTheme()
     const currentTheme = theme === "system" ? systemTheme : theme
-    const [navbar, setNavbar] = useState(false)
+
+    useEffect(() => {setMounted(true)}, [])
+    
+    if(!mounted){
+        return null
+    }
+
     return (
-        <header className={`w-full px-32 py-10 font-medium flex max-md:flex-col max-md:space-y-6 flex-row items-center justify-between shadow fixed z-50 top-0 ${theme === 'dark' ? 'bg-black' : 'bg-white'}`}>
+        <header className={`w-full px-32 py-10 font-medium flex max-md:flex-col max-md:space-y-6 flex-row items-center justify-between shadow fixed z-50 top-0 ${currentTheme === 'dark' ? 'bg-black' : 'bg-white'}`}>
             <div className='flex flex-row justify-between max-md:w-full'>
                 <h2>Logo</h2>
                 <div className="md:hidden">
