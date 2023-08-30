85b4a3cb545ec2419db1c0cda6de9baabd297e3d
JJayJohnny <jasiek.barczewski@gmail.com>
2023-08-30 15:05:10 +0200

    added animations to project cards

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,12 +5,13 @@ import DeleteButton from "./DeleteButton";
 import {DiGithubBadge} from "react-icons/di"
 import {CgWebsite} from "react-icons/cg"
 import { useSession } from 'next-auth/react'
+import { motion } from 'framer-motion'
 
 const ProjectCard = ({project}) => {
     const {data: session} = useSession()
     return(
-        <div className="flex flex-row justify-between md:w-1/2 w-4/5 mx-auto space-x-5 shadow-xl rounded-xl bg-gray-200 dark:bg-gray-800">
-            <div className="w-1/2"><Image src={project.imagePath} width={300} height={300}/></div>
+        <motion.div className="flex flex-row justify-between md:w-1/2 w-4/5 mx-auto space-x-5 shadow-xl rounded-xl bg-gray-200 dark:bg-gray-800" initial={{x: "-100%"}} whileInView={{x: 0, transition:{duration: 0.5, delay: 0.5}}} viewport={{once: true}} whileHover={{scale: 1.05}}>
+            <div className="w-1/2"><Image src={project.imagePath} width={300} height={300} className="rounded-l-xl"/></div>
             <div className="justify-start text-start w-1/2">
                 <h3 className="font-semibold text-xl">{project.title}</h3>
                 <p>{project.description}</p>
@@ -20,7 +21,7 @@ const ProjectCard = ({project}) => {
                     {session ? <DeleteButton id={project._id}/> : null }
                 </div>
             </div>          
-        </div>
+        </motion.div>
     )
 }
 
