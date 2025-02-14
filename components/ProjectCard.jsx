307782a5cb07642e38c377b62d5be4e13da7eff4
JJayJohnny <jasiek.barczewski@gmail.com>
2025-02-14 20:31:09 +0100

    fix project card responsiveness

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -10,21 +10,21 @@ import { motion } from 'framer-motion'
 const ProjectCard = ({project, idx}) => {
     const {data: session} = useSession()
     return(
-        <motion.div className="flex flex-row justify-between lg:w-1/2 w-4/5 mx-auto space-x-5 rounded-xl bg-gray-300/80 dark:bg-gray-800/60 my-5" 
+        <motion.div className="flex lg:flex-row flex-col justify-between xl:w-7/12 w-4/5 mx-auto space-x-5 rounded-xl bg-gray-300/80 dark:bg-gray-800/60" 
         initial={{x: idx%2 == 0 ? "-100%" : "100%", opacity: 0}} 
         whileInView={{x: 0, opacity: 1,
             transition:{duration: 0.5, delay: 0.5}
         }} 
         viewport={{once: true}} 
         whileHover={{scale: 1.05}}>
-            <div className="w-1/2"><img src={project.imageName} width={300} height={300} className="rounded-l-xl" alt={`Image of ${project.title}`}/></div>
-            <div className="text-start w-1/2 flex flex-col justify-between p-5">
+            <div className="lg:w-1/2 w-full"><img src={project.imageName} className="lg:rounded-l-xl lg:h-full max-lg:w-full max-lg:rounded-t-xl" alt={`Image of ${project.title}`}/></div>
+            <div className="text-start lg:w-1/2 w-auto flex flex-col justify-between p-5">
                 <div>
-                <div className="flex flex-row">
+                <div className="flex flex-row mb-2">
                     <h3 className="font-semibold text-xl">{project.title}</h3>
                     <h3 className="text-xl ml-auto text-purple-500">{project.year}</h3>
                 </div>
-                <p>{project.description}</p>
+                <div className="mb-2"><p>{project.description}</p></div>
                 </div>
                 <div className="flex flex-row justify-start space-x-2">
                     {project.github ? <a href={project.github} target="_blank"><DiGithubBadge size={28}/></a> : null }
