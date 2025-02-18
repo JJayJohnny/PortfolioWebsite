949866ddc4d23ff198c577fd3ac085777f35afac
JJayJohnny <jasiek.barczewski@gmail.com>
2025-02-18 13:23:24 +0100

    fix key error and add second course

--- components/Education.jsx
+++ components/Education.jsx
@@ -10,8 +10,20 @@ import pgLogoDark from '../public/Logo_Gdansk_Tech_Dark.svg'
 const COURSES = [
     {
         name: 'Informatics',
+        specialty: 'High Performance Computing and Computational Intelligence',
+        degree: 'Master of Engineering',
+        school: 'Gdańsk Tech',
+        duration: 'Feb. 2025 - now',
+        grade: '-',
+        gpa: '-',
+        url: 'https://ects.pg.edu.pl/en/courses/17348',
+        img: pgLogo,
+        imgDark: pgLogoDark,
+    },
+    {
+        name: 'Informatics',
         specialty: 'Computer Systems Architecture',
-        degree: 'engineer',
+        degree: 'Engineer',
         school: 'Gdańsk Tech',
         duration: 'Oct. 2021 - Feb. 2025',
         grade: '5',
@@ -32,13 +44,13 @@ const Education = () => {
             <div className="flex flex-col space-y-5 overflow-hidden py-3">
                 {COURSES.map((course, idx) => {
                     return (
-                        <motion.div className="mx-auto rounded-xl bg-gray-300/80 dark:bg-gray-800/60 w-4/5 lg:w-1/2 xl:w-2/6 p-5" whileHover={{scale: 1.05}}>
+                        <motion.div className="mx-auto rounded-xl bg-gray-300/80 dark:bg-gray-800/60 w-4/5 lg:w-1/2 xl:w-2/6 p-5" key={idx} whileHover={{scale: 1.05}}>
                             <div className="flex sm:flex-row flex-col mb-2">
                                 <a href={course.url} target="_blank"><h3 className="font-semibold text-xl">{course.name} <FiExternalLink className="inline" size={15}/></h3></a>
                                 <h3 className="font-semibold text-xl sm:ml-auto text-purple-500">{course.duration}</h3>
                             </div>
                             <div className="flex flex-row justify-between">
-                                <div>
+                                <div className='w-4/6'>
                                     <div><span className='underline'>Specialty</span>: {course.specialty}</div>
                                     <div><span className='underline'>Degree</span>: {course.degree}</div>
                                     <div><span className='underline'>School</span>: {course.school}</div>
